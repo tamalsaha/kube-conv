551f8b3cd4f2f096e0a959c0e3848e8afe6bfe48
tamal <tamal@appscode.com>
2018-03-17 08:47:46 -0700

    added-all

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"k8s.io/apimachinery/pkg/runtime"
 	// batchv1 "k8s.io/api/batch/v1"
 	// batchv1beta1 "k8s.io/api/batch/v1beta1"
 	"github.com/appscode/go/log"
@@ -21,11 +22,187 @@ import (
 	//	"k8s.io/apimachinery/pkg/runtime"
 	"bytes"
 	"fmt"
+	"io"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/kubernetes/pkg/apis/apps"
 )
 
+var ss = `{
+  "apiVersion": "apps/v1",
+  "kind": "StatefulSet",
+  "metadata": {
+    "creationTimestamp": null
+  },
+  "spec": {
+    "selector": null,
+    "serviceName": "",
+    "template": {
+      "metadata": {
+        "creationTimestamp": null
+      },
+      "spec": {
+        "containers": null
+      }
+    },
+    "updateStrategy": {}
+  },
+  "status": {
+    "observedGeneration": 1,
+    "replicas": 0
+  }
+}`
+
+func transform(obj runtime.Object) (runtime.Object, error) {
+	var n int32 = 2
+	r := obj.(*v1beta1.StatefulSet)
+	r.Spec.Replicas = &n
+	return obj, nil
+}
+
+func xyz() runtime.Codec {
+	mediaType := "application/json"
+	info, ok := runtime.SerializerInfoForMediaType(legacyscheme.Codecs.SupportedMediaTypes(), mediaType)
+	if !ok {
+		panic("unsupported media type " + mediaType)
+	}
+	return info.Serializer
+}
+
+var Codec = xyz()
+
+type VersionedCodec struct {
+	encodeVersion runtime.GroupVersioner
+	decodeVersion runtime.GroupVersioner
+}
+
+func (c VersionedCodec) Encode(obj runtime.Object, w io.Writer) error {
+	internal, err := legacyscheme.Scheme.UnsafeConvertToVersion(obj, runtime.InternalGroupVersioner)
+	if err != nil {
+		return err
+	}
+
+	out, err := legacyscheme.Scheme.UnsafeConvertToVersion(internal, c.encodeVersion)
+	if err != nil {
+		return err
+	}
+	legacyscheme.Scheme.Default(out)
+
+	return Codec.Encode(out, w)
+}
+
+func (c VersionedCodec) Decode(data []byte, _ *schema.GroupVersionKind, _ runtime.Object) (runtime.Object, *schema.GroupVersionKind, error) {
+	in, gvk, err := Codec.Decode(data, nil, nil)
+	if err != nil {
+		return nil, gvk, err
+	}
+	legacyscheme.Scheme.Default(in)
+
+	internal, err := legacyscheme.Scheme.UnsafeConvertToVersion(in, runtime.InternalGroupVersioner)
+	if err != nil {
+		return nil, gvk, err
+	}
+
+	out, err := legacyscheme.Scheme.UnsafeConvertToVersion(internal, c.decodeVersion)
+	return out, gvk, err
+}
+
 func main() {
+	raw := []byte(ss)
+
+	c := VersionedCodec{
+		encodeVersion: v1.SchemeGroupVersion,
+		decodeVersion: v1beta1.SchemeGroupVersion,
+	}
+	cur, gvk, err := c.Decode(raw, nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(gvk)
+
+	mod, err := transform(cur)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+	err = c.Encode(mod, &buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(buf.String())
+}
+
+func main3() {
+	// legacyscheme.Codecs.CodecForVersions()
+
+	raw := []byte(ss)
+
+	srcObj, srcGVK, err := Codec.Decode(raw, nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(srcGVK)
+	legacyscheme.Scheme.Default(srcObj)
+
+	srcInternal, err := legacyscheme.Scheme.UnsafeConvertToVersion(srcObj, runtime.InternalGroupVersioner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dstObj, err := legacyscheme.Scheme.UnsafeConvertToVersion(srcInternal, v1beta1.SchemeGroupVersion)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dstMod, err := transform(dstObj)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dstModInternal, err := legacyscheme.Scheme.UnsafeConvertToVersion(dstMod, runtime.InternalGroupVersioner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srcMod, err := legacyscheme.Scheme.UnsafeConvertToVersion(dstModInternal, v1.SchemeGroupVersion)
+	if err != nil {
+		log.Fatal(err)
+	}
+	legacyscheme.Scheme.Default(srcMod)
+
+	var mod bytes.Buffer
+	err = Codec.Encode(srcMod, &mod)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mod.String())
+
+	//codec := legacyscheme.Codecs.CodecForVersions(Codec, Codec, nil, nil)
+	//obj, gvk, err := codec.Decode(raw, nil,  nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(gvk)
+
+	//codec := legacyscheme.Codecs.CodecForVersions(info.Serializer, info.Serializer, v1beta1.SchemeGroupVersion, nil)
+	//v1Obj := &v1.StatefulSet{
+	//	Status: v1.StatefulSetStatus{
+	//		ObservedGeneration: 1,
+	//	},
+	//}
+	//var b1 bytes.Buffer
+	//err := codec.Encode(v1Obj, &b1)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(b1.String())
+
+	// legacyscheme.Codecs.LegacyCodec(legacyscheme.Registry.EnabledVersions()...)
+}
+
+func main2() {
 	v1Obj := &v1.StatefulSet{
 		Status: v1.StatefulSetStatus{
 			ObservedGeneration: 1,
@@ -33,20 +210,20 @@ func main() {
 	}
 
 	/*
-	// working example:
+		// working example:
 
-	// src -> internal -> dst
-	internalObj := &apps.StatefulSet{}
-	err := legacyscheme.Scheme.Convert(v1Obj, internalObj, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+		// src -> internal -> dst
+		internalObj := &apps.StatefulSet{}
+		err := legacyscheme.Scheme.Convert(v1Obj, internalObj, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	v1beta1Obj := &v1beta1.StatefulSet{}
-	err = legacyscheme.Scheme.Convert(internalObj, v1beta1Obj, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+		v1beta1Obj := &v1beta1.StatefulSet{}
+		err = legacyscheme.Scheme.Convert(internalObj, v1beta1Obj, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	*/
 
 	var err error
@@ -115,7 +292,7 @@ func main() {
 	//}
 }
 
-type GroupVersions struct {}
+type GroupVersions struct{}
 
 // KindForGroupVersionKinds identifies the preferred GroupVersionKind out of a list. It returns ok false
 // if none of the options match the group.
